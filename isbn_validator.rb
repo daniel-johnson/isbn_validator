c4f60b74fe208be9ce31bb59cc6c03092e36147c
Daniel Johnson <janiel.dohnson@gmail.com>
2019-05-06 19:35:28 +0000

    Add new route: /service/:input/validate. Route takes input, returns a string.

--- isbn_validator.rb
+++ isbn_validator.rb
@@ -4,3 +4,8 @@ require 'sinatra/reloader' if development?
 get '/' do
   'Hello world!'
 end
+
+get '/service/:input/validate' do
+  response_body = "Input is '#{params['input']}'."
+  return [200, {}, response_body]
+end
