073db3074e88740eab2db3360b1c8fb716020d4d
Daniel Johnson <janiel.dohnson@gmail.com>
2019-05-06 21:10:18 +0000

    Add multiple changes: Handle three cases of input, and output the appropriate http code and message for each. Change response body to json.

--- isbn_validator.rb
+++ isbn_validator.rb
@@ -10,6 +10,16 @@ end
 get '/service/:input/validate' do |input|
   content_type(:json, charset: 'utf-8')
 
-  response_body = "Input is '#{input}'."
-  return [200, {}, response_body]
+  http_code, message =
+    if !input.match?(/[0-9]{13}/)
+      [400, "'#{input}' is not a valid ISBN number. Correct format is an integer with 13 digits"]
+    elsif check_digit_invalid?(input)
+      [400, "ISBN code invalid. Check digit is not correct."]
+    else
+      [200, "ISBN #{input} is valid."]
+    end
+
+  response_body = {'message': message}.to_json
+
+  return [http_code, response_body]
 end
