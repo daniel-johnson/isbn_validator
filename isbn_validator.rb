5887e367acc00f25faa64f995eb29a0d7510ba1b
Daniel Johnson <janiel.dohnson@gmail.com>
2019-05-06 20:52:28 +0000

    Change the content type to json and utf-8.

--- isbn_validator.rb
+++ isbn_validator.rb
@@ -6,6 +6,8 @@ get '/' do
 end
 
 get '/service/:input/validate' do |input|
+  content_type(:json, charset: 'utf-8')
+
   response_body = "Input is '#{input}'."
   return [200, {}, response_body]
 end
